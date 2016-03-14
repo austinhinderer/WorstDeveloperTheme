f18e00ff09344d05132c5b0a18b028a609c31cb1
Austin Hinderer <zeshon@gmail.com>
2016-03-14 15:09:35 -0700

    Fixes #8 and updates colors

--- WorstTheme/404.php
+++ WorstTheme/404.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
-			
+
 	<div id="content">
 
+		<?php get_template_part( 'parts/content', 'logo' ); ?>
+
 		<div id="inner-content" class="row">
-	
+
 			<main id="main" class="large-8 medium-8 columns" role="main">
 
 				<article id="content-not-found">
-				
+
 					<header class="article-header">
 						<h1><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
 					</header> <!-- end article header -->
-			
+
 					<section class="entry-content">
 						<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></p>
 					</section> <!-- end article section -->
@@ -19,9 +21,9 @@
 					<section class="search">
 					    <p><?php get_search_form(); ?></p>
 					</section> <!-- end search section -->
-			
+
 				</article> <!-- end article -->
-	
+
 			</main> <!-- end #main -->
 
 		</div> <!-- end #inner-content -->
